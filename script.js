64f458458efcc837afa2f64e2f86c28850664fef
Jaleron <jaleronmail@gmail.com>
2022-11-19 23:05:58 -0500

    created function that creates table data

--- script.js
+++ script.js
@@ -2,21 +2,40 @@ const stateNames = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'C
 const stateAbbreviations = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
 let stateLinks = [];
 
-for (let i = 0; i < stateNames.length; i++) {
-	stateLinks.push(`https://www.${stateAbbreviations[i].toLowerCase()}.gov`);
-	createLI(stateLinks[i]);
-}
+function createTableData(stateName, fullLink, abbreviatedLink) {
+	const table = document.querySelector('table');
+	const tr = document.createElement('tr');
+	const td1 = document.createElement('td');
+	const td2 = document.createElement('td');
+	const td3 = document.createElement('td');
 
-function createLI(stateLink) {
-	const li = document.createElement('li');
-	const a = document.createElement('a');
-	
-	const node = document.createTextNode(stateLink);
-	a.appendChild(node);
-	a.href = stateLink;
-	a.target = '_blank';
-	li.appendChild(a);
-	
-	const ul = document.querySelector('ul');
-	ul.appendChild(li);
+	const column1 = document.createTextNode(stateName);
+	const column2 = document.createTextNode(fullLink);
+	const column3 = document.createTextNode(abbreviatedLink);
+
+	td1.append(column1);
+	td2.append(column2);
+	td3.append(column3);
+	tr.append(td1, td2, td3);
+	table.append(tr);
 }
+createTableData(1, 2, 3);
+
+// function createLI(stateLink) {
+// 	const li = document.createElement('li');
+// 	const a = document.createElement('a');
+	
+// 	const node = document.createTextNode(stateLink);
+// 	a.appendChild(node);
+// 	a.href = stateLink;
+// 	a.target = '_blank';
+// 	li.appendChild(a);
+	
+// 	const ul = document.querySelector('ul');
+// 	ul.appendChild(li);
+// }
+
+// for (let i = 0; i < stateNames.length; i++) {
+// 	stateLinks.push(`https://www.${stateAbbreviations[i].toLowerCase()}.gov`);
+// 	createLI(stateLinks[i]);
+// }
