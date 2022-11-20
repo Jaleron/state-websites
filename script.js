eead0f0d163cfbeee466df528c99e3d43163484c
Jaleron <jaleronmail@gmail.com>
2022-11-19 21:22:49 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const stateNames = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+const stateAbbreviations = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+let stateLinks = [];
+
+for (let i = 0; i < stateAbbreviations.length; i++) {
+	stateLinks.push(`https://www.${stateAbbreviations[i].toLowerCase()}.gov`);
+}
+
+function createLI(stateLink) {
+	const li = document.createElement('li');
+	const a = document.createElement('a');
+	
+	const node = document.createTextNode(stateLink);
+	a.appendChild(node);
+	a.href = stateLink;
+	a.target = '_blank';
+	li.appendChild(a);
+	
+	const ul = document.querySelector('ul');
+	ul.appendChild(li);
+}
+
+for (let i = 0; i < stateLinks.length; i++) {
+	createLI(stateLinks[i]);
+}
